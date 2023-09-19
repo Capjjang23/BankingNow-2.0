b4cd047d88d9b72d27cb652c919c6255d301faf5
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-09-20 04:27:03 +0900

    temp

--- app/src/main/java/com/example/bankingnow/ui/RemitAccountDialog.kt
+++ app/src/main/java/com/example/bankingnow/ui/RemitAccountDialog.kt
@@ -117,6 +117,7 @@ class RemitAccountDialog : BaseDialogFragment<DialogRemitAccountBinding>(R.layou
                         RemitBankDialog().show(parentFragmentManager,"송금 계좌")
                         dismiss()
                     } else if (state=="SUCCESS" && distanceX < -100){
+
                         // 왼쪽으로 스와이프
                         if (remitResultIsFill)
                             setFragmentResult("Check", bundleOf("isFill" to true))
